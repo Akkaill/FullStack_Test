5de27a9dd333289f1962145c61ad5b0555317fe3
IKKYU <kunlaphat_@hotmail.com>
2025-09-19 04:40:07 +0700

    Prepare for production BE&FE

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,8 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors({
-    origin: [/localhost:\d+$/, 'https://your-frontend-domain.com'],
-    credentials: true,
+    origin: [
+      'http://localhost:5173',
+      'https://full-stack-test-green.vercel.app', 
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    
   });
   await app.listen(3000);
 }
