0a4305bc12c8d679907921d9d8306cc913a08c75
IKKYU <kunlaphat_@hotmail.com>
2025-09-19 04:13:33 +0700

    add SpeedInsights

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,38 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import AppLayout from './ui/AppLayout'
-import StudentsPage from './pages/StudentPage'
-import RoomsPage from './pages/RoomsPage'
-import RoomDetailPage from './pages/RoomDetailPage'
-import ReportsPage from './pages/ReportsPage'
-import 'antd/dist/reset.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AppLayout from "./ui/AppLayout";
+import StudentsPage from "./pages/StudentPage";
+import RoomsPage from "./pages/RoomsPage";
+import RoomDetailPage from "./pages/RoomDetailPage";
+import ReportsPage from "./pages/ReportsPage";
+import "antd/dist/reset.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const qc = new QueryClient()
+const qc = new QueryClient();
 const router = createBrowserRouter([
-  { path: '/', element: <AppLayout />,
+  {
+    path: "/",
+    element: <AppLayout />,
     children: [
-      { index: true, element: <StudentsPage/> },
-      { path: 'students', element: <StudentsPage/> },
-      { path: 'rooms', element: <RoomsPage/> },
-      { path: 'rooms/:id', element: <RoomDetailPage/> },
-      { path: 'reports', element: <ReportsPage/> },
+      { index: true, element: <StudentsPage /> },
+      { path: "students", element: <StudentsPage /> },
+      { path: "rooms", element: <RoomsPage /> },
+      { path: "rooms/:id", element: <RoomDetailPage /> },
+      { path: "reports", element: <ReportsPage /> },
     ],
   },
-])
+]);
 
-const container = document.getElementById('root')!
-
+const container = document.getElementById("root")!;
 
 const root =
   (window as any).__appRoot ??
-  ((window as any).__appRoot = ReactDOM.createRoot(container))
+  ((window as any).__appRoot = ReactDOM.createRoot(container));
 
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={qc}>
       <RouterProvider router={router} />
+      <SpeedInsights />
     </QueryClientProvider>
   </React.StrictMode>
-)
+);
